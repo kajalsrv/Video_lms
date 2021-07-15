1a7033cf0c215bd42490727129154305f6994e4c
kajalsrv <45989701+kajalsrv@users.noreply.github.com>
2021-07-15 15:08:44 +0530

    Add files via upload

--- ExceptionInterface.php
+++ ExceptionInterface.php
@@ -1,10 +1,22 @@
 <?php
 
-namespace Stripe\Exception\OAuth;
+namespace Stripe\Exception;
 
-/**
- * The base interface for all Stripe OAuth exceptions.
- */
-interface ExceptionInterface extends \Stripe\Exception\ExceptionInterface
-{
+// TODO: remove this check once we drop support for PHP 5
+if (\interface_exists(\Throwable::class, false)) {
+    /**
+     * The base interface for all Stripe exceptions.
+     */
+    interface ExceptionInterface extends \Throwable
+    {
+    }
+} else {
+    /**
+     * The base interface for all Stripe exceptions.
+     */
+    // phpcs:disable PSR1.Classes.ClassDeclaration.MultipleClasses
+    interface ExceptionInterface
+    {
+    }
+    // phpcs:enable
 }
